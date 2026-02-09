5ef2ea2b96f02356c27079aa2a8f3d5d419c1be0
yi1lei6a3 <yi1lei6a3@gmail.com>
2026-02-10 01:13:14 +0300

    fix install

--- install.sh
+++ install.sh
@@ -1,41 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-echo "[*] System Audit Tool installation started"
-
+REPO_URL="https://github.com/DevvIlya/system_audit.git"
 INSTALL_DIR="/opt/system-audit"
 VENV_DIR="$INSTALL_DIR/venv"
 BIN_PATH="/usr/local/bin/system-audit"
 
-# Проверка Python
-if ! command -v python3 &>/dev/null; then
-    echo "[!] Python3 not found"
-    exit 1
-fi
+echo "[*] System Audit Tool installation started"
 
-echo "[*] Creating install directory: $INSTALL_DIR"
-sudo mkdir -p $INSTALL_DIR
+# checks
+command -v git >/dev/null || { echo "[!] git not found"; exit 1; }
+command -v python3 >/dev/null || { echo "[!] python3 not found"; exit 1; }
 
-echo "[*] Copying project files"
-sudo cp -r audit.py utils config.yaml requirements.txt $INSTALL_DIR
+echo "[*] Installing to $INSTALL_DIR"
+sudo rm -rf "$INSTALL_DIR"
+sudo git clone "$REPO_URL" "$INSTALL_DIR"
 
-# Virtualenv
-if [ ! -d "$VENV_DIR" ]; then
-    echo "[*] Creating virtual environment"
-    sudo python3 -m venv $VENV_DIR
-fi
+cd "$INSTALL_DIR"
 
-echo "[*] Installing Python dependencies"
-sudo $VENV_DIR/bin/pip install --upgrade pip
-sudo $VENV_DIR/bin/pip install -r $INSTALL_DIR/requirements.txt
+echo "[*] Creating virtual environment"
+python3 -m venv venv
 
-echo "[*] Creating CLI command"
-sudo tee $BIN_PATH > /dev/null <<EOF
+echo "[*] Installing dependencies"
+"$VENV_DIR/bin/pip" install --upgrade pip
+"$VENV_DIR/bin/pip" install -r requirements.txt
+
+echo "[*] Creating CLI command: system-audit"
+sudo tee "$BIN_PATH" > /dev/null <<EOF
 #!/usr/bin/env bash
 exec $VENV_DIR/bin/python $INSTALL_DIR/audit.py "\$@"
 EOF
 
-sudo chmod +x $BIN_PATH
+sudo chmod +x "$BIN_PATH"
 
 echo "[+] Installation completed successfully"
-echo "Run: sudo system-audit"
+echo "Run: system-audit"
